538fd42be6a08d994c19f0ffbf3568e67fcf9c61
MEEZA453 <157560850+MEEZA453@users.noreply.github.com>
2025-03-13 11:01:43 +0530

    co

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect(process.env.MONGO_URL_PROD)
         .then(()=>{console.log('DB Conncted🎈')})
     }catch (error) {
             console.log ('DB Error😴:', error)
